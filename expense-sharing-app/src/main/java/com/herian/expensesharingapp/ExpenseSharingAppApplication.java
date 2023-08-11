db145d5c4c214f0e3ddd84294b82d031e24d32f3
Petr Herian <petr.herian@zentity.com>
2023-08-11 12:12:40 +0200

    initial spring boot project

---
+++ expense-sharing-app/src/main/java/com/herian/expensesharingapp/ExpenseSharingAppApplication.java
@@ -0,0 +1,13 @@
+package com.herian.expensesharingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSharingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseSharingAppApplication.class, args);
+	}
+
+}
---
+++ expense-sharing-app/src/test/java/com/herian/expensesharingapp/ExpenseSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herian.expensesharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
